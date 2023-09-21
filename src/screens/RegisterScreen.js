5517f4877bf8d978f79e67da286e37a866d15eb0
Amar-cmd <amarentp23@gmail.com>
2023-09-21 14:53:40 +0530

    regster screen build

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -53,34 +53,47 @@ const RegisterScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Image source={samvada_logo} style={styles.image} resizeMode="contain" />
-      <Text style={styles.heading}>Create Account</Text>
-      <View style={styles.nameInputContainer}>
-        <Text style={styles.nameInitial}>{getInitials(name)}</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Name"
-          placeholderTextColor="#888"
-          onChangeText={handleNameChange}
-          value={name}
+      <View style={styles.topSection}>
+        <Image
+          source={samvada_logo}
+          style={styles.image}
+          resizeMode="contain"
         />
       </View>
 
-      <View style={styles.phoneInputContainer}>
-        <Text style={styles.countryCode}>{countryCode}</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Phone Number"
-          placeholderTextColor="#888"
-          keyboardType="phone-pad"
-          onChangeText={handlePhoneNumberChange}
-          value={phoneNumber}
-        />
+      <View style={styles.inputSection}>
+        <Text style={styles.heading}>Create Account</Text>
+        <View style={styles.nameInputContainer}>
+          <Text style={styles.nameInitial}>{getInitials(name)}</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Name"
+            placeholderTextColor="#888"
+            onChangeText={handleNameChange}
+            value={name}
+          />
+        </View>
+
+        <View style={styles.phoneInputContainer}>
+          <Text style={styles.countryCode}>{countryCode}</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Phone Number"
+            placeholderTextColor="#888"
+            keyboardType="phone-pad"
+            onChangeText={handlePhoneNumberChange}
+            value={phoneNumber}
+          />
+        </View>
       </View>
 
-      <TouchableOpacity style={styles.customButton} onPress={handleCreatePress}>
-        <Text style={styles.buttonText}>Create</Text>
-      </TouchableOpacity>
+      <View style={styles.bottomSection}>
+        <TouchableOpacity
+          style={styles.customButton}
+          onPress={handleCreatePress}>
+          <Text style={styles.buttonText}>Continue</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -89,17 +102,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
     padding: 20,
+    justifyContent: 'space-between',
+  },
+  topSection: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  inputSection: {
+    flex: 2,
+    justifyContent: 'center',
+  },
+  bottomSection: {
+    flex: 1,
+    justifyContent: 'flex-end',
   },
   image: {
-    width: '100%',
+    width: '60%',
     height: 150,
     marginBottom: 30,
-    marginTop: 20,
+    alignSelf: 'center',
   },
   heading: {
-    color: '#000',
+    color: '#6A5BC2',
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    letterSpacing:1,
   },
   phoneInputContainer: {
     flexDirection: 'row',
@@ -112,25 +144,28 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   countryCode: {
+    flex: 1, // 10% width
     fontSize: 16,
     marginRight: 10,
     padding: 10,
     color: '#000',
     borderWidth: 1,
     borderColor: '#6A5BC2',
+    textAlign: 'center',
   },
   nameInitial: {
+    flex: 1, // 10% width
     fontSize: 16,
     marginRight: 10,
     padding: 10,
     color: '#000',
     borderWidth: 1,
     borderColor: '#6A5BC2',
+    textAlign: 'center',
   },
   input: {
-    width: '85%',
+    flex: 9, // 90% width
     height: 45,
-    borderColor: 'gray',
     paddingLeft: 10,
     color: '#000',
     fontSize: 16,
